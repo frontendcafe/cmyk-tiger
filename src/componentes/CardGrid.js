0f2d0c815efd46577f456d56c410ef271b8d4073
Milagros Alfonzo <milagrosalfonzo@outlook.com.ar>
2020-12-10 16:24:19 -0300

    comentarios implm card

--- src/componentes/CardGrid.js
+++ src/componentes/CardGrid.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import { GridListTile, GridListTileBar } from "@material-ui/core";
-import CardMovie from "./MovieCard";
+//import CardMovie from "./MovieCard";
 
 const CardGrid = ({ movies }) => {
   return (
     <Container>
+      {/* en el caso de que la card se implemente junto en conjunto a la grid se
+      remplaza con este codigo, que le pasa como prop la info de movie a la card
+      {movies.map((tile) => (
+      <CardMovie movie={tile} key={tile.id} /> */}
+
       {movies.map((tile) => (
         <GridListTile key={tile.title}>
-          {/* {movies.map((tile) => (
-            <CardMovie movie={tile} key={tile.id} />
-          ))} */}
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${tile.poster_path}`}
+            alt={tile.title}
+          />
           <GridListTileBar title={tile.title} />
         </GridListTile>
       ))}
